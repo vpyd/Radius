23fab5976e70056201eeb058a7768e017711724e
vpyd <71273037+vpyd@users.noreply.github.com>
2020-09-14 20:24:12 +0530

    Find the area of the circle
    Finding the area of the circle with radius given. programming done in python developed by Vishnupriya Y D

---
+++ abc.py
@@ -0,0 +1,6 @@
+# area of a circle with radius input
+radius = float(input("Input the radius of the circle : "))
+import math
+Area = float(math.pi* (radius*radius))
+print("The area of the circle with radius 1.1 is :",Area)
+
